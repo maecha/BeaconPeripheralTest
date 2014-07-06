1c80e0aff36ead5b656f9754061b6a88c73058d8
Seiya <s.maezono.73@gmail.com>
2014-07-06 11:09:29 +0900

    uuid, major, minor を設定できるように変更

--- BeaconPeripheralTest/ViewController.m
+++ BeaconPeripheralTest/ViewController.m
@@ -16,18 +16,21 @@
 #define UUID @"096781F6-8F73-41D2-A768-FB45AE14BDC9"
 #define MAJOR 3
 #define MINOR 10
+#define MEASURED_POWER -59
 // Beacon信号に含まれず、領域識別のための内部管理用の識別子
 #define IDENTIFER @"info.maezono.beacontest"
 
-@interface ViewController () <CBPeripheralManagerDelegate>
+@interface ViewController () <CBPeripheralManagerDelegate, UITextFieldDelegate>
 
 @property(nonatomic, strong) NSUUID *proximityUUID;
 @property(nonatomic, strong) CBPeripheralManager *peripheralManager;
 
 @property (nonatomic, weak) IBOutlet UILabel *stateLabel;
-@property (nonatomic, weak) IBOutlet UILabel *uuidLabel;
-@property (nonatomic, weak) IBOutlet UILabel *majorNumLabel;
-@property (nonatomic, weak) IBOutlet UILabel *minorNumLabel;
+@property (nonatomic, weak) IBOutlet UITextField *uuidTextField;
+@property (nonatomic, weak) IBOutlet UITextField *majorNumTextField;
+@property (nonatomic, weak) IBOutlet UITextField *minorNumTextField;
+@property (nonatomic, weak) IBOutlet UIButton *startAdvertisingButton;
+@property (nonatomic, weak) IBOutlet UIButton *stopAdvertisingButton;
 
 @end
 
@@ -45,17 +48,14 @@
     
     self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
     
-    // 各信号の情報をラベルへ適用させる
-    self.uuidLabel.text = UUID;
-    self.majorNumLabel.text = [NSString stringWithFormat:@"%d", MAJOR];
-    self.minorNumLabel.text = [NSString stringWithFormat:@"%d", MINOR];
+    // 各信号の情報をテキストフィールドへ適用させる
+    self.uuidTextField.text = UUID;
+    self.majorNumTextField.text = [NSString stringWithFormat:@"%d", MAJOR];
+    self.minorNumTextField.text = [NSString stringWithFormat:@"%d", MINOR];
     
-    // アドバタイズ開始処理
-    // CBPeripheralManagerState の状態は Bluetoothの状態（電源のOn,Offやアプリケーションに実行権限があるかなど）を確認してくれる。
-    if (self.peripheralManager.state == CBPeripheralManagerStatePoweredOn) {
-        
-        [self startAdvertising];
-    }
+    // 停止ボタン無効化
+    self.stopAdvertisingButton.enabled = NO;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -66,12 +66,17 @@
 
 -(void)startAdvertising
 {
-    NSLog(@"startAdvertising");
+    NSLog(@"startAdvertising...");
     
-    CLBeaconRegion *beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:self.proximityUUID
-                                                                           major: MAJOR
-                                                                           minor: MINOR
+    // 文字列をUUIDに変換
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:self.uuidTextField.text];
+    
+    CLBeaconRegion *beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID: uuid
+                                                                           major: [self.majorNumTextField.text intValue]
+                                                                           minor: [self.minorNumTextField.text intValue]
                                                                       identifier: IDENTIFER];
+    
+    NSLog(@"%@ - %d - %d", uuid, [self.majorNumTextField.text intValue], [self.minorNumTextField.text intValue]);
     
     NSDictionary *beaconPeripheralData = [beaconRegion peripheralDataWithMeasuredPower:nil];
     
@@ -84,7 +89,7 @@
     
     if (error) {
         
-        [self resetLabels];
+        [self resetLabelAndTextFields];
         
         NSLog(@"Failed to start advertising with error:%@", error);
     }
@@ -109,7 +114,7 @@
         {
             stateStr = @"PoweredOn";
             
-            [self startAdvertising];
+            //[self startAdvertising];
             
             break;
         }
@@ -146,13 +151,62 @@
     
 }
 
+#pragma mark - UITextField Delegate
 
-- (void)resetLabels {
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    // キーボードを隠す
+    [self.view endEditing:YES];
+    
+    return YES;
+}
+
+#pragma mark - IBAction job
+
+// アドバタイズ開始
+-(IBAction)startAdvertising:(id)sender{
+    
+    // アドバタイズ開始処理
+    // CBPeripheralManagerState の状態は Bluetoothの状態（電源のOn,Offやアプリケーションに実行権限があるかなど）を確認してくれる。
+    if (self.peripheralManager.state == CBPeripheralManagerStatePoweredOn) {
+        
+        // 開始ボタン無効化
+        self.startAdvertisingButton.enabled = NO;
+        // 停止ボタン有効化
+        self.stopAdvertisingButton.enabled = YES;
+        
+        [self startAdvertising];
+    }
+    
+}
+
+// アドバタイズ停止
+-(IBAction)stopAdvertising:(id)sender{
+    
+    // アドバタイズ停止
+    self.peripheralManager.delegate = nil;
+    [self.peripheralManager stopAdvertising];
+    [self resetLabelAndTextFields];
+    
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                     queue:nil
+                                                                   options:nil];
+    
+    // 開始ボタン有効化
+    self.startAdvertisingButton.enabled = YES;
+    // 停止ボタン無効化
+    self.stopAdvertisingButton.enabled = NO;
+    
+}
+
+#pragma mark -
+
+- (void)resetLabelAndTextFields {
     
     self.stateLabel.text = @"---";
-    self.uuidLabel.text = nil;
-    self.majorNumLabel.text = nil;
-    self.minorNumLabel.text = nil;
+    self.uuidTextField.text = UUID;
+    self.majorNumTextField.text = self.majorNumTextField.text;
+    self.minorNumTextField.text = self.minorNumTextField.text;
     
 }
 
